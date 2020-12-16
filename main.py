644c2436d8f94fb5ef7dd60b3ee0085fc0d932c6
hashiguchi1544 <hashiguchi1544@gmail.com>
2020-12-16 16:14:47 +0900

    db作成

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import sqlite3
+from pathlib import Path
+
+
+
+def input_task():
+    print('入力してください')
+
+
+def db_init():
+    root_path = Path(__file__).parent
+
+    db_path = root_path / 'sample.db'
+
+    conn = sqlite3.connect(db_path)
+    conn.close
+
 def main():
+    db_init()
     while True:
         print('1 : 入力する')
         print('2 : タクスを確認する')
@@ -6,7 +24,7 @@ def main():
         num = int(input())
 
         if num == 1:
-            pass
+            input_task()
         elif num == 2:
             pass
         elif num == 3:
