ba4e3c4c53b6b02c2276cd7a3fbe2ca5e5e7aebc
hashiguchi1544 <hashiguchi1544@gmail.com>
2020-12-16 13:14:00 +0900

    入力画面を作成した

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def main():
-    pass
+    while True:
+        print('1 : 入力する')
+        print('2 : タクスを確認する')
+        print('3 : 終了する')
+        num = int(input())
+
+        if num == 1:
+            pass
+        elif num == 2:
+            pass
+        elif num == 3:
+            exit()
 
 
 if __name__ == '__main__':
