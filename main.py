fd0cdeb30365ab06c1ad8ec23850e3c07c61f53c
hashiguchi1544 <hashiguchi1544@gmail.com>
2020-12-16 19:47:27 +0900

    とりあえず基本機能は完成

--- main.py
+++ main.py
@@ -2,31 +2,68 @@ import sqlite3
 from pathlib import Path
 
 
-
-def input_task():
-    print('入力してください')
-
-
 def db_init():
     root_path = Path(__file__).parent
 
     db_path = root_path / 'sample.db'
 
     conn = sqlite3.connect(db_path)
-    conn.close
+    conn.close()
+
+
+def write_task():
+    conn = sqlite3.connect('sample.db')
+    task = input('タスクを入力してください')
+
+    # sql = f'INSERT INTO my_task (task) VALUES ("{task}");'
+    sql = f'INSERT INTO my_task (task) VALUES (?);'
+
+    conn.execute(sql, (task,))
+    conn.commit()
+
+    conn.close()
+
+
+def read_tasks():
+    conn = sqlite3.connect('sample.db')
+
+    sql = 'SELECT id, task FROM my_task;'
+
+    results = conn.execute(sql).fetchall()
+    conn.close()
+    return results
+
+
+def delete_task():
+    num = int(input('完了した番号を入力指定ください'))
+    conn = sqlite3.connect('sample.db')
+
+    sql = f'DELETE FROM my_task WHERE id = ?;'
+
+    conn.execute(sql, (num,))
+    conn.commit()
+    conn.close()
+
 
 def main():
     db_init()
     while True:
-        print('1 : 入力する')
-        print('2 : タクスを確認する')
-        print('3 : 終了する')
+        results = read_tasks()
+        print('--------------------------------------------------')
+        for id_int, task in results:
+            print(f'{id_int}, {task}')
+
+
+        print('--------------------------------------------------')
+        print('1:完了しましたぜ  2:タスクを追加する  3:終了する')
+
         num = int(input())
 
+
         if num == 1:
-            input_task()
+            delete_task()
         elif num == 2:
-            pass
+            write_task()
         elif num == 3:
             exit()
 
